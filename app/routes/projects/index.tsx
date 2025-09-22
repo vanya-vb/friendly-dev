eff5703067797816cd9b89926fb67edf102e1c2f
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-22 21:35:56 +0300

    fetch data with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,15 @@
-export default function ProjectsPage() {
+import type { Route } from "./+types/index";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+    const res = await fetch("http://localhost:8000/projects");
+    const data = await res.json();
+
+    return { projects: data };
+}
+
+export default function ProjectsPage({ loaderData }: Route.ComponentProps) {
+    const { projects } = loaderData;
+
     return (
         <>
             <h2 className="text-3xl font-bold text-white mb-8">
