0f9603671f94b7b54329df1a1d687f90fb5eee64
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-23 08:50:53 +0300

    display loader data

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 
+import ProjectCard from "~/components/ProjectCard";
+
 export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
     const res = await fetch("http://localhost:8000/projects");
     const data = await res.json();
@@ -13,9 +15,12 @@ export default function ProjectsPage({ loaderData }: Route.ComponentProps) {
 
     return (
         <>
-            <h2 className="text-3xl font-bold text-white mb-8">
-                Projects
-            </h2>
+            <h2 className="text-3xl font-bold text-white mb-8">Projects</h2>
+            <div className="grid gap-6 sm:grid-cols-2">
+                {projects.map(project => (
+                    <ProjectCard key={project.id} project={project} />
+                ))}
+            </div>
         </>
     );
 };
