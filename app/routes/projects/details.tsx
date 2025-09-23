2fdfadc3fc2b611193824c8abbfff3c0b4d2edef
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-23 09:13:20 +0300

    add details page

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/details";
+import { Link } from "react-router";
 import type { Project } from "~/types";
+import { FaArrowLeft } from "react-icons/fa";
 
 export async function clientLoader({ request, params }: Route.ClientLoaderArgs): Promise<Project> {
     const res = await fetch(`http://localhost:8000/projects/${params.id}`);
@@ -19,6 +21,27 @@ export default function ProjectDetailsPage({ loaderData }: Route.ComponentProps)
     const project = loaderData;
 
     return (
-        <>Project Details</>
+        <>
+            <Link to="/projects" className="flex items-center text-blue-400 hover:text-blue-500 mb-6 transition">
+                <FaArrowLeft className="mr-2" /> Back to Projects
+            </Link>
+
+            <div className="grid gap-8 md:grid-cols-2 items-start">
+                <div>
+                    <img src={project.image} alt={project.title} className="w-full rounded-lg shadow-md" />
+                </div>
+
+                <div>
+                    <h1 className="text-3xl font-bold text-blue-400 mb-4">{project.title}</h1>
+                    <p className="text-gray-300 text-sm mb-4">
+                        {new Date(project.date).toLocaleDateString()} - {project.category}
+                    </p>
+                    <p className="text-gray-200 mb-6">{project.description}</p>
+                    <a href={project.url} target="_blank" className="inline-block text-white bg-blue-600 hover:to-blue-700 px-6 py-2 rounded transition">
+                        View Live Site
+                    </a>
+                </div>
+            </div>
+        </>
     );
 };
