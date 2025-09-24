1f19759367fff736c2b8789dc3c014fd9ea1f521
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-24 12:27:53 +0300

    add not found page

--- app/routes.ts
+++ app/routes.ts
@@ -10,5 +10,6 @@ export default [
         route("projects", "./routes/projects/index.tsx"),
         route("projects/:id", "./routes/projects/details.tsx"),
         route("blog", "./routes/blog/index.tsx"),
+        route("*", "./routes/errors/not-found.tsx")
     ]),
 ] satisfies RouteConfig;
