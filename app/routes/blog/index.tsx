71ab3943d8a6258ceb454ee91ae8ed730f328c9b
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-25 12:49:46 +0300

    add pagination

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
+
 import PostCard from "~/components/PostCard";
+import Pagination from "~/components/Pagination";
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
     const url = new URL("/posts-meta.json", request.url);
@@ -18,14 +21,32 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 
 export default function BlogPage({ loaderData }: Route.ComponentProps) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 3;
+
     const { posts } = loaderData;
+
+    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
 
     return (
         <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8">Blog</h2>
-            {posts.map(post => (
+            {currentPosts.map(post => (
                 <PostCard key={post.slug} post={post} />
             ))}
+
+            {
+                totalPages > 1 && (
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        onPageChange={(page) => setCurrentPage(page)}
+                    />
+                )
+            }
         </div>
     );
 };
