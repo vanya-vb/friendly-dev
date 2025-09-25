82f12776c23bbec609c40263adbcb0425033df1e
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-25 13:03:42 +0300

    add filtering

---
+++ app/components/PostFilter.tsx
@@ -0,0 +1,18 @@
+type PostFilterProps = {
+    searchQuery: string;
+    onSearchChange: (value: string) => void
+}
+
+export default function PostFilter({ searchQuery, onSearchChange }: PostFilterProps) {
+    return (
+        <div className="mb-6">
+            <input
+                type="text"
+                value={searchQuery}
+                onChange={(e) => onSearchChange(e.target.value)}
+                placeholder="Search Posts..."
+                className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:otline-none focus:ring-2 focus:ring-blue-500"
+            />
+        </div>
+    );
+};
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -4,6 +4,7 @@ import type { PostMeta } from "~/types";
 
 import PostCard from "~/components/PostCard";
 import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
     const url = new URL("/posts-meta.json", request.url);
@@ -21,22 +22,47 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 
 export default function BlogPage({ loaderData }: Route.ComponentProps) {
+    const [searchQuery, setSearchQuery] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
+
     const postsPerPage = 3;
 
     const { posts } = loaderData;
 
-    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const filteredPosts = posts.filter(post => {
+        const query = searchQuery.toLowerCase();
+
+        return (
+            post.title.toLowerCase().includes(query) || post.excerpt.toLowerCase().includes(query)
+        )
+    })
+
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
     const indexOfLast = currentPage * postsPerPage;
     const indexOfFirst = indexOfLast - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+    const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
 
     return (
         <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8">Blog</h2>
-            {currentPosts.map(post => (
-                <PostCard key={post.slug} post={post} />
-            ))}
+
+            <PostFilter
+                searchQuery={searchQuery}
+                onSearchChange={(query) => {
+                    setSearchQuery(query);
+                    setCurrentPage(1);
+                }}
+            />
+
+            <div className="space-y-8">
+                {currentPosts.length === 0 ? (
+                    <p className="text-gray-400 text-center">No posts found</p>
+                ) : (
+                    currentPosts.map(post => (
+                        <PostCard key={post.slug} post={post} />
+                    ))
+                )}
+            </div>
 
             {
                 totalPages > 1 && (
